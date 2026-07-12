7f1a8bc1c93cad446b77355326bc69ba3bcfe875
Vaibhav <vaibhavkumarnigam30@gmail.com>
2026-07-12 19:10:53 +0530

    Adding Backend URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const API_URL =
     window.location.hostname === "localhost"
         ? "http://127.0.0.1:8000"
-        : "https://your-backend.onrender.com";
+        : "https://developer-debugging-assistant-be.onrender.com";
